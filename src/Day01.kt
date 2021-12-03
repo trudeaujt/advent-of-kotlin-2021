88fd3d357afa057a19fd7bc5aa68f3eaa88727c6
Jonathan Trudeau <trudeaujt@gmail.com>
2021-12-03 15:35:11 +0900

    Solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.windowed(2, 1) //split into pairs
+            .count{ it[1] > it[0] } //count pairs where second is greater than first
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return input.windowed(3, 1) //split into sets of 3
+             { it.sum() } //sum each set
+            .windowed(2, 1) //split into pairs
+            .count{ it[1] > it[0] } //count pairs where second is greater than first
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInputInt("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInputInt("Day01")
     println(part1(input))
     println(part2(input))
 }
