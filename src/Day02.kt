9725d325e8ee64533ea3742cd42253e43a2ef527
Jonathan Trudeau <trudeaujt@gmail.com>
2021-12-07 15:51:47 +0900

    Solve day 2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,36 +1,48 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        class Submarine (var position: Int, var depth: Int) {
-            fun move(direction: String, distance: Int) {
-                when (direction) {
-                    "forward" -> position += distance
-                    "down" -> depth += distance
-                    "up" -> depth -= distance
+    class Submarine (var position: Int, var depth: Int, var aim: Int) {
+        fun move(direction: String, distance: Int) {
+            when (direction) {
+                "forward" -> position += distance
+                "down" -> depth += distance
+                "up" -> depth -= distance
+            }
+        }
+        fun aimAndMove(direction: String, distance: Int) {
+            when (direction) {
+                "down" -> aim += distance
+                "up" -> aim -= distance
+                "forward" -> {
+                    position += distance
+                    depth += aim * distance
                 }
             }
-            fun multiplyPosAndDepth(): Int {
-                return position * depth
-            }
         }
-        val sub = Submarine(0, 0)
-        val list = input
-            .map{ it.split(" ") }
+        fun multiplyPosAndDepth(): Int {
+            return position * depth
+        }
+    }
 
-        list.forEach {
-            sub.move(it[0], it[1].toInt())
-        }
+    fun part1(input: List<String>): Int {
+        val sub = Submarine(0, 0, 0)
+        input
+            .map{ it.split(" ") }
+            .forEach { sub.move(it[0], it[1].toInt()) }
         return sub.multiplyPosAndDepth()
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val sub = Submarine(0, 0, 0)
+        input
+            .map{ it.split(" ") }
+            .forEach { sub.aimAndMove(it[0], it[1].toInt()) }
+        return sub.multiplyPosAndDepth()
     }
 
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 150)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 900)
 
     val input = readInput("Day02")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
